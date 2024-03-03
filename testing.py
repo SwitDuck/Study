879dde4b658ba17eda916cf37d7f739ad7f730df
SwitDuck <BaD4epT@gmail.com>
2024-03-03 19:55:59 +1000

    update4.1

--- testing.py
+++ testing.py
@@ -8,6 +8,7 @@ F = open('datafile.pkl','rb')
 E = pickle.load(F)
 print(E)
 '''
+'''
 import struct
 F = open('data.bin', 'wb')
 data = struct.pack('>i4sh', 7, b'spam', 8)
@@ -18,3 +19,12 @@ data = F.read()
 F.close()
 values = struct.unpack('>i4sh', data)
 print(values)
+'''
+'''
+X = [1, 2, 3]
+L = ['a', X, 'b']
+D = {'x':X,'y':2}
+X[1]='surprise'
+print(L)
+print(D)
+'''
